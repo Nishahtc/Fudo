2b6e21a2caf45b7766d3d4aaff524ca901361f0d
Nishahtc <nishamodi254@email.com>
2025-07-24 13:13:25 +0530

    header footer completed

--- javascript/script.js
+++ javascript/script.js
@@ -1,19 +1,8 @@
 const searchIcon = document.getElementById("searchIcon");
-const searchBox = document.querySelector(".search-box");
-const searchCloseBtn = document.getElementById("searchCloseBtn");
+const searchBox = document.getElementById("searchBoxWrapper");
 
 searchIcon.addEventListener("click", function (e) {
   e.preventDefault();
   searchBox.classList.toggle("d-none");
-  setTimeout(() => {
-    searchBox.classList.toggle("active");
-  }, 10);
-});
-
-searchCloseBtn.addEventListener("click", function () {
-  searchBox.classList.remove("active");
-  setTimeout(() => {
-    searchBox.classList.add("d-none");
-  }, 300);
 });
 
